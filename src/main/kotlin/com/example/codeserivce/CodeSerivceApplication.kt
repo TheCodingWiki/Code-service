16ce2be47e805ccec2f6189684157eb0d9865da7
Semm6 <sem.dewilde6@gmail.com>
2022-01-25 16:39:59 +0100

    First commit

---
+++ src/main/kotlin/com/example/codeserivce/CodeSerivceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.codeserivce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeSerivceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodeSerivceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/codeserivce/CodeSerivceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.codeserivce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CodeSerivceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
